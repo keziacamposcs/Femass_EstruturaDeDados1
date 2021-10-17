36e48af111db559d19b55d779dda81a82638f89b
Kezia Campos <keziacamposcs@gmail.com>
2021-10-17 15:02:17 -0300

    17.10.21

--- Diego/EXERCÍCIO 2/vetor.h
+++
@@ -1,3 +0,0 @@
-void mostra_vetor(int* v, int tam);
-void mostra_vetor_impares(int* v, int tam);
-void mostra_vetor_pares(int* v, int tam);
--- Diego/EXERCÍCIO 4/maquina_de_escrever.h
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <stdlib.h>	/* Para usar malloc e free */
-using namespace std;
-
-char* cadastrar_novo_caractere(char *texto, int *total_elementos, char c);
-void mostrar_texto(char *texto, int mum_elementos);

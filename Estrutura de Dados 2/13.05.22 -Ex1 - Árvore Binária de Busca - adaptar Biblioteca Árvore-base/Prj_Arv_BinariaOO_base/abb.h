95d9d009a7d6f2887238e84914bcdc71c2cac5bd
Kezia Campos <keziacamposcs@gmail.com>
2022-05-03 14:48:31 -0300

    03.05.22

---
+++ Estrutura de Dados 2/13.05.22 -Ex1 - Árvore Binária de Busca - adaptar Biblioteca Árvore-base/Prj_Arv_BinariaOO_base/abb.h
@@ -0,0 +1,58 @@
+//abb.h
+//Define a classe e protótipo de métodos OO para biblioteca de árvore binária de busca (abb)
+//Por Irineu Lima, ED2 - FeMASS
+
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <stack> //aplicada para função abb_displayTree - contâiner C++ LIFO
+
+using namespace std;
+
+struct noArv  //definição struct para nó-dado de árvore binária de busca
+{
+	int info;
+	struct noArv* esq; //ponteiro para filho à esquerda
+	struct noArv* dir; //ponteiro para filho à direita
+};
+typedef struct noArv NoArv; //definição de tipo para nó-dado de árvore binária de busca
+
+class ArvBB{
+	//Declarar atributos
+	//public:
+	private:
+		NoArv* root; //controle de ponteiro para nó-dado raiz
+	
+	//Declarar cabeçalhos de métodos
+	public:
+		ArvBB(); //Construtor - método de mesmo nome da classe, não se usa a previsão de retorno
+		~ArvBB(); //Destrutor - método executado ao fim ou término de uso do objeto de classe
+		
+		void insere(int v);
+		void displayTree();
+		NoArv* getRoot();
+		void reiniciaTree();
+		bool vazia();
+		void displayRoot();
+		
+		//continuacao
+		int nivel();
+		
+		
+		
+	private:
+		NoArv* abb_criavazia();
+		NoArv* abb_cria (int c, NoArv* e, NoArv* d);
+		NoArv* abb_insere(NoArv* a, int v);
+		NoArv* abb_libera(NoArv* a);
+		bool no_abb_vazio(NoArv* a);
+		
+		//continuacao
+		NoArv* abb_nivel(NoArv* a);
+		
+		
+		
+}; //toda classe encerra com ; (padrão da linguagem)
+
---
+++ Estrutura de Dados 2/13.05.22 -Ex1 - Árvore Binária de Busca - adaptar Biblioteca Árvore-base/Prj_Arv_BinariaOO_base/main.cpp
@@ -0,0 +1,141 @@
+#include "abb.h"
+#include <conio.h>
+using namespace std;
+
+/* Prof. Irineu - Est. de Dados
+Projeto de base para implementação de Árvore Binária de Busca (AVL). Supõe-se:
+- main.cpp: front-end com usuário;
+- abb.h e abb.cpp: prototipação OO de métodos de manipulação da árvore binária de busca (abb),
+ contendo definição "struct" do nó-dado (campos info, filho_esquerda e filho_direita; como 
+ também os métodos de manipulação como biblioteca OO.
+Dica: referência de muitos métodos em: 
+ https://www.ic.unicamp.br/~rocha/teaching/2016s1/mc202/aulas/arvore-binaria-busca-01.pdf  
+ Livro Robert Lafore, ED e Alg em Java - Ciência Moderna, 2004.
+Obs.: Recursos e métodos adicionais à biblioteca deverão ser desenvolvidos com base neste modelo.
+*/
+ 
+//função de interface com o usuário, apenas para facilitar mensagens sobre o estado da árvore 
+void mensagem_arvore_vazia(bool t){
+	cout << "Árvore está ";
+	if(t)
+		printf("vazia\n");
+	else
+		printf("nao-vazia\n");
+}
+
+int menu()
+{
+	system("cls");
+	int op;
+	printf("***Menu***\n");
+	printf("1 - Insere elemento;\n");
+	printf("2 - Imprime a arvore;\n");
+	printf("3 - Reinicia arvore;\n");
+	printf("4 - Imprime a raiz;\n");
+	printf("5 - Quantidade de niveis;\n");
+	printf("6 - Quantidade de nos;\n");
+	printf("7 - Travessia pre-ordem;\n");
+	printf("8 - Travessia em-ordem;\n");
+	printf("9 - Travessia pos-ordem;\n");
+	printf("10 - Balancear;\n");
+
+	printf("11 - Sair\n");
+	
+	printf("Escolha umas das opcoes acima: ");
+	scanf("%d", &op);
+	return op;
+}
+
+int main(int argc, char** argv) {
+	
+	setlocale(LC_ALL, "Portuguese");
+	printf("PROGRAMA ÁRVORE BINÁRIA DE BUSCA OO\n");
+
+	ArvBB tree; //instancia objeto conforme a classe, construtor faz root = NULL
+	
+	mensagem_arvore_vazia(tree.vazia());
+	
+	int op;
+	int num; //elemento informado pelo usuario
+	do
+	{
+		op=menu();
+		switch(op)
+		{
+			
+			case 1:
+				printf("Numero para inserir: \n");
+				scanf("%d", &num);
+				
+				tree.insere(num);
+				
+				system("PAUSE");
+				break;
+				
+			case 2:
+				tree.displayTree();
+				
+				system("PAUSE");
+				break;
+				
+			case 3:
+				printf("Reiniciando a árvore!\n");
+				tree.reiniciaTree();
+				mensagem_arvore_vazia(tree.vazia());
+				
+				system("PAUSE");
+				break;
+
+			case 4:
+				tree.displayRoot();
+				
+				system("PAUSE");
+				break;
+				
+			case 5:
+				tree.nivel();
+				
+				system("PAUSE");
+				break;
+			case 6:
+				
+				
+				system("PAUSE");
+				break;
+				
+			case 7:
+				
+				
+				system("PAUSE");
+				break;
+				
+			case 8:
+				
+				
+				system("PAUSE");
+				break;
+				
+			case 9:
+				
+				
+				system("PAUSE");
+				break;
+				
+			case 10:
+			
+			
+				system("PAUSE");
+				break;
+				
+			default:
+				system("cls");
+				printf("Voce digitou %d. Opcao invalida.", op);
+				system("PAUSE");					
+		}
+	}
+	while(op!=11);
+	tree.~ArvBB(); //destrutor da árvore
+
+	return 0;
+}
+
